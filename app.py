51b8d0065b503c6d8ffc94913140059fa005ab92
Michael Jamesley <124603498+mikej3663@users.noreply.github.com>
2025-05-01 09:46:40 -0400

    Update app.py

--- app.py
+++ app.py
@@ -42,8 +42,8 @@ df_prices = pd.DataFrame({
 })
 
 # --- Calculate Cumulative Returns ---
-df_prices['Actual_Cumulative'] = ((1 + df_prices['Actual']/100).cumprod() - 1)
-df_prices['Predictions_Cumulative'] = ((1 + df_prices['Predictions']/100).cumprod() - 1)
+df_prices['Actual_Cumulative'] = ((1 + df_prices['Actual']/100).cumprod())
+df_prices['Predictions_Cumulative'] = ((1 + df_prices['Predictions']/100).cumprod())
 
 # --- Plot: Cumulative Actual vs Cumulative Predictions ---
 st.subheader("Cumulative Predictions vs Actual")
